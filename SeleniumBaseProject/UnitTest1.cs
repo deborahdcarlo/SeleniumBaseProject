b58453788b74b429acfa1ec1bc191a1c2f7ed113
Deborah De Oliveira <deborahdcarlo@gmail.com>
2020-04-15 19:42:35 +0200

    More base tests

--- SeleniumBaseProject/UnitTest1.cs
+++ SeleniumBaseProject/UnitTest1.cs
@@ -2,13 +2,14 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Data;
 using System.Threading;
 
 namespace SeleniumBaseProject
 {
     public class Tests
     {
-        
+
 
         [SetUp]
         public void Setup()
@@ -18,7 +19,7 @@ namespace SeleniumBaseProject
         [Test]
         public void Test1()
         {
-            
+
             var driver = GetDriver();
             driver.Navigate().GoToUrl("https://ultimateqa.com/simple-html-elements-for-automation");
 
@@ -30,7 +31,7 @@ namespace SeleniumBaseProject
 
             IWebElement dropdown = driver.FindElement(By.XPath("//select/option[contains(text(),'Audi')]"));
             dropdown.Click();
-   
+
             var salary = By.XPath("//*[@id='htmlTableId']//td[contains(text(), '$150,000+')]");
             HighlightElementUsingJavaScript(salary, driver);
 
@@ -42,6 +43,65 @@ namespace SeleniumBaseProject
             var location = By.XPath("//*[@class='et_pb_column et_pb_column_1_3 et_pb_column_10 et_pb_css_mix_blend_mode_passthrough']");
 
             HighlightElementUsingJavaScript(location, driver);
+        }
+
+        [Test]
+        [Category("Navigation")]
+        public void Test2()
+        {
+            var driver = GetDriver();
+
+            driver.Navigate().GoToUrl("https://ultimateqa.com/");
+            Assert.AreEqual("Home - Ultimate QA", driver.Title);
+
+            driver.Navigate().GoToUrl("https://ultimateqa.com/automation");
+            Assert.AreEqual("Automation Practice - Ultimate QA", driver.Title);
+
+            IWebElement hrefLocator = driver.FindElement(By.XPath("//*[@href='../complicated-page']"));
+            hrefLocator.Click();
+            Assert.AreEqual("Complicated Page - Ultimate QA", driver.Title);
+
+            driver.Navigate().Back();
+            Assert.AreEqual("Automation Practice - Ultimate QA", driver.Title);
+
+        }
+
+        [Test]
+        [Category("Manipulation")]
+        public void Test3()
+        {
+            var driver = GetDriver();
+            driver.Navigate().GoToUrl("https://ultimateqa.com/filling-out-forms");
+
+            var nameField = driver.FindElement(By.Id("et_pb_contact_name_1"));
+            nameField.Clear();
+            nameField.SendKeys("Deborah");
+
+            var textField = driver.FindElement(By.Id("et_pb_contact_message_1"));
+            textField.Clear();
+            textField.SendKeys("Hi");
+
+            //var captcha = driver.FindElement(By.XPath("//*[@class='et_pb_contact_captcha_question']/following-sibling::input/@data-first_digit"));
+            var captcha = driver.FindElement(By.XPath("//*[@class='et_pb_contact_captcha_question']")).Text;
+            captcha = captcha.Replace(" ", "");
+            var numbers = captcha.Split('+');
+
+            //another way
+            //var table = new DataTable();
+            //var captchaAnswer = (int)table.Compute(captcha, "");
+
+
+            var total = Convert.ToInt32(numbers[0]) + Convert.ToInt32(numbers[1]);
+
+            var totalField = driver.FindElement(By.XPath("//*[@class='et_pb_contact_captcha_question']/following-sibling::input"));
+            totalField.Clear();
+            totalField.SendKeys(total.ToString());
+
+            var submit = driver.FindElements(By.Name("et_builder_submit_button"));
+            submit[1].Click();
+
+            var successMessage = driver.FindElement(By.ClassName("et-pb-contact-message")).FindElement(By.TagName("p"));
+            Assert.IsTrue(successMessage.Text.Equals("Success"));
         }
 
         private IWebDriver GetDriver()
